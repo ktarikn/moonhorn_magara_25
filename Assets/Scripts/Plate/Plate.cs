7a901a5ccece4ef5ccbce5a47c68e0b2b2e73cc7
Serkan Kılıç <01serkan8@gmail.com>
2025-09-19 23:20:16 +0300

    plate

---
+++ Assets/Scripts/Plate/Plate.cs
@@ -0,0 +1,107 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Plate : MonoBehaviour
+{
+    // --- ESKÝ DEÐÝÞKENLER AYNI KALIYOR ---
+    [Header("Görsel Ayarlar")]
+    [SerializeField] private Color normalColor = Color.white;
+    [SerializeField] private Color pressedColor = Color.green;
+
+    [Header("Hareket Ayarlarý")]
+    [SerializeField] private float pressedOffsetY = -0.2f;
+    [SerializeField] private float moveSpeed = 5f;
+
+    [Header("Baðlantýlar")]
+    [SerializeField] private Transform buttonTransform;
+
+    // --- YENÝ DEÐÝÞKENÝ EKLEYELÝM ---
+    [Header("Kontrol Edilecek Obje")]
+    [Tooltip("Bu plaka basýldýðýnda kontrol edilecek kapý")]
+    [SerializeField] private Door doorToControl;
+
+    private SpriteRenderer buttonSpriteRenderer;
+    private Vector3 originalPosition;
+    private Vector3 targetPosition;
+    private List<Rigidbody2D> objectsOnPlate = new List<Rigidbody2D>();
+
+    void Start()
+    {
+        // Start metodu ayný kalýyor
+        if (buttonTransform != null)
+        {
+            originalPosition = buttonTransform.position;
+            targetPosition = originalPosition;
+            buttonSpriteRenderer = buttonTransform.GetComponent<SpriteRenderer>();
+        }
+        else
+        {
+            Debug.LogError("Button Transform atanmamýþ!", this.gameObject);
+        }
+        SetColor();
+    }
+
+    void FixedUpdate()
+    {
+        // FixedUpdate metodu ayný kalýyor
+        if (objectsOnPlate.Count > 0)
+        {
+            targetPosition = originalPosition + new Vector3(0, pressedOffsetY, 0);
+        }
+        else
+        {
+            targetPosition = originalPosition;
+        }
+        buttonTransform.position = Vector3.Lerp(buttonTransform.position, targetPosition, Time.fixedDeltaTime * moveSpeed);
+    }
+
+    // --- AddObject ve RemoveObject METOTLARINI GÜNCELLEYELÝM ---
+
+    public void AddObject(Rigidbody2D rb)
+    {
+        if (!objectsOnPlate.Contains(rb))
+        {
+            // Eðer bu, plakanýn üzerine konan ÝLK nesne ise...
+            if (objectsOnPlate.Count == 0 && doorToControl != null)
+            {
+                // Kapýya açýl komutu gönder
+                doorToControl.OpenDoor();
+            }
+
+            objectsOnPlate.Add(rb);
+            SetColor();
+        }
+    }
+
+    public void RemoveObject(Rigidbody2D rb)
+    {
+        if (objectsOnPlate.Contains(rb))
+        {
+            objectsOnPlate.Remove(rb);
+
+            // Eðer bu nesne kalktýktan sonra plakanýn üzerinde HÝÇ nesne kalmadýysa...
+            if (objectsOnPlate.Count == 0 && doorToControl != null)
+            {
+                // Kapýya kapan komutu gönder
+                doorToControl.CloseDoor();
+            }
+            SetColor();
+        }
+    }
+
+    private void SetColor()
+    {
+        // SetColor metodu ayný kalýyor
+        if (buttonSpriteRenderer == null) return;
+
+        if (objectsOnPlate.Count > 0)
+        {
+            buttonSpriteRenderer.color = pressedColor;
+        }
+        else
+        {
+            buttonSpriteRenderer.color = normalColor;
+        }
+    }
+}
